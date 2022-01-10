94518f324b44303f5a63c0ed0dd15aeea8bd4127
rajarshix1 <rajarshim5@gmail.com>
2022-01-10 18:10:59 +0530

    1-3 - 1-5

--- Part-1/testapp/src/components/Total.js
+++ Part-1/testapp/src/components/Total.js
@@ -1,6 +1,6 @@
 const Total = (props) => {
     return (
-        <p>Total number of exercise is {props.total}</p>
+        <p>Total number of exercise is {props.total[0].exercises+props.total[1].exercises+props.total[2].exercises}</p>
     )
 }
 export default Total

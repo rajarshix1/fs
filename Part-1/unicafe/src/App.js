d4409e7c4104372beba51380f28928261664ead3
rajarshix1 <rajarshim5@gmail.com>
2022-01-14 16:39:12 +0530

    anecdotes created

--- Part-1/unicafe/src/App.js
+++ Part-1/unicafe/src/App.js
@@ -1,30 +1,59 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
+const Statistics = ({ good, neutral, bad }) => {
+  const StatisticLine = ({ text, value }) => (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+  return good !== 0 || bad !== 0 || neutral !== 0 ? (
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine
+          text="average"
+          value={(good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad)}
+        />
+        <StatisticLine
+          text="positive"
+          value={(100 * good) / (good + neutral + bad) + "%"}
+        />
+      </tbody>
+    </table>
+  ) : (
+    <div>Nothing to Display</div>
+  );
+};
 
-const Statistics=({good,neutral,bad})=>(
- good !=0 || bad!=0 ||neutral!=0? <div>
- <p>good {good}</p>
- <p>neutral {neutral}</p>
- <p>bad {bad}</p>
- <p>average {(good*1+neutral*0+bad*(-1))/(good+neutral+bad)}</p>
- <p>positive {100*good/(good+neutral+bad)}%</p>
-</div> : <div>Nothing to display</div>
-)
+const StatisticLine = ({ text, value }) => (
+  <p>
+    {text} {value}
+  </p>
+);
+
+const Button = ({ text, setFeedback }) => (
+  <button onClick={setFeedback}>{text}</button>
+);
+
 const App = () => {
   // save clicks of each button to its own state
-  let [good, setGood] = useState(0)
-  let [neutral, setNeutral] = useState(0)
-  let [bad, setBad] = useState(0)
+  let [good, setGood] = useState(0);
+  let [neutral, setNeutral] = useState(0);
+  let [bad, setBad] = useState(0);
 
   return (
     <div>
-<h1> give feedback</h1>
-<button onClick={()=>setGood(good++)}>good</button>
-<button onClick={()=>setNeutral(neutral++)}>neutral</button>
-<button onClick={()=>setBad(bad++)}>bad</button>
-    <Statistics good={good} neutral={neutral} bad={bad}/>
+      <h1> give feedback</h1>
+      <Button setFeedback={() => setGood((good += 1))} text="good" />
+      <Button setFeedback={() => setNeutral((neutral += 1))} text="neutral" />
+      <Button setFeedback={() => setBad((bad += 1))} text="bad" />
+      <br/>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- Part-1/unicafe/src/components/History.js
+++
@@ -1,4 +0,0 @@
-const History=(props)=>(
-    props.lorr.length===0? <div>nothing</div> : <div>{props.lorr.join(' ')}</div>
-)
-export default History
